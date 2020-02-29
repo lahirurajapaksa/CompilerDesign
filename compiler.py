bd693685fd56538828207e1042de9dac31bc1aff
lahirurajapaksa <lahiru9954@gmail.com>
2020-02-29 20:38:43 +0000

    Works, need to add better error checking and splitting of data

--- compiler.py
+++ compiler.py
@@ -1,52 +1,12 @@
 def readinput():
-	#fh = open("example.txt","r")
-
-	#read the file line by line
-	# with open("example.txt") as filein:
-	# 	lines=[]
-	# 	for line in filein:
-	# 		#examine the line individually
-	# 		#print(line)
-	# 		#strip the line of whitespace and trailing newline char
-	# 		line.strip()
-	# 		line = line.strip('\n')
-
-	# 		# #split the line based on the colon
-	# 		# colonSplit = line.split(":")
-
-	# 		# print("colonSplit is ", colonSplit)
-			
-
-	# 		# if colonSplit[0]=="variables":
-	# 		# 	variables = colonSplit[1]
-	# 		# elif colonSplit[0]=="constants":
-	# 		# 	constants = colonSplit[1]
-	# 		# elif colonSplit[0]=="predicates":
-	# 		# 	predicates = colonSplit[1]
-	# 		# elif colonSplit[0]=="equality":
-	# 		# 	equality = colonSplit[1]
-	# 		# elif colonSplit[0]=="connectives":
-	# 		# 	connectives = colonSplit[1]
-	# 		# elif colonSplit[0]=="quantifiers":
-	# 		# 	quantifiers = colonSplit[1]
-
-	# 		# #there can be tabs, line breaks and spaces between symbols in the formula
-	# 		# elif colonSplit[0]=="formula":
-	# 		# 	formula = colonSplit[1]
-	# 		# else:
-	# 		# 	print("wrong input")
-
-
-
-	# 		lines.append(line)
 
 	#store the file in a list, with each new line as a new element
 	lines = list(open("example.txt"))
-	validsets = ["variables","constants","predicates","equality","connectives","quantifiers"]
+	validsets = ["variables:","constants:","predicates:","equality:","connectives:","quantifiers:"]
 	i=0
-	formulaFound=False
 	while i<(len(lines)):
 	
+		formulaFound=False
 
 		currentline = lines[i]
 		print(currentline)
@@ -55,39 +15,12 @@ def readinput():
 		currentline = currentline.strip('\n')
 		currentline = currentline.strip('\t')
 
-
+		print("currentline is ",currentline)
 		 #split the line based on the colon
 		colonSplit = currentline.split(":")
 
+
 		print("colonSplit is ", colonSplit)
-
-		#check whether we are dealing with 'formuala', if so we need to check the next lines as well
-		#use a while loop
-		if colonSplit[0]=='formula':
-			formulaFound=True
-			#add the current line's data to the formula
-			formula = colonSplit[1]
-
-		if formulaFound==True:
-			stop = False
-			i=i+1
-			while stop==False:
-				nextline = lines[i]
-				nextline = nextline.strip('\n')
-				nextline = nextline.strip('\t')
-
-				#split the line in to several elements
-
-				nextlinesplit = nextline.split()
-
-				if nextlinesplit[0] in validsets:
-					#the next line is not part of the formula
-					i=i-1
-					stop = True
-				else:
-					#add it to the formula
-					formula = formula + str(nextline)
-					i=i+1
 
 		if colonSplit[0]=="variables":
 			variables = colonSplit[1]
@@ -107,36 +40,72 @@ def readinput():
 		elif colonSplit[0]=="quantifiers":
 		 	quantifiers = colonSplit[1]
 		 	i=i+1
+		elif colonSplit[0]=="formula":
+		#check whether we are dealing with 'formuala', if so we need to check the next lines as well
+			formulaFound=True
+		#add the current line's data
+			formula=colonSplit[1]
+		else:
+			print("invalid maate")
+			print("this is it ", colonSplit)
+
+		#use a while loop
+
+		if formulaFound==True:
+			stop = False
+			i=i+1
+			print("i+1 is ",i,"before entering while loop")
+			while stop==False and i<len(lines):
+				nextline = lines[i]
+				nextline = nextline.strip('\n')
+				nextline = nextline.strip('\t')
+
+				#split the line in to several elements
+
+				nextlinesplit = nextline.split()
+				print("first element of next line is ",nextlinesplit[0])
+				if nextlinesplit[0] in validsets:
+					#the next line is not part of the formula
+					#i=i-1
+					print("next line is not part of formula so i is ",i)
+					stop = True
+				elif nextlinesplit[0] not in validsets:
+					#add it to the formula
+					formula = formula + str(nextline)
+					i=i+1
+					print("adding to formula so i is",i)
 
 
 
 
 	#separate the contents in to individual elements in a list structure
-	# variables = variables.strip()
-	# variables = variables.split(' ')
-	# print("variables =", variables)
+	variables = variables.strip()
+	variables = variables.split(' ')
+	print("variables =", variables)
 
-	# constants = constants.strip()
-	# constants = constants.split(' ')
-	# print("constants =", constants)
+	constants = constants.strip()
+	constants = constants.split(' ')
+	print("constants =", constants)
 
-	# predicates = predicates.strip()
-	# predicates = predicates.split(' ')
-	# print("predicates =", predicates)
+	predicates = predicates.strip()
+	predicates = predicates.split(' ')
+	print("predicates =", predicates)
 
-	# equality = equality.strip()
-	# equality = equality.split(' ')
-	# print("equality =", equality)
+	equality = equality.strip()
+	equality = equality.split(' ')
+	print("equality =", equality)
 
-	# connectives = connectives.strip()
-	# connectives = connectives.split(' ')
-	# print("connectives =", connectives)
+	connectives = connectives.strip()
+	connectives = connectives.split(' ')
+	print("connectives =", connectives)
 
-	# quantifiers = quantifiers.strip()
-	# quantifiers = quantifiers.split(' ')
-	# print("quantifiers =", quantifiers)
+	quantifiers = quantifiers.strip()
+	quantifiers = quantifiers.split(' ')
+	print("quantifiers =", quantifiers)
 
-	# print("formula =", formula)
+	formula = formula.strip()
+	formula = formula.split(' ')
+	print("formula =", formula)
 
 
 
