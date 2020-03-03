8d88728cac893a3e3d07aac93c364e77a9cdc8c2
lahirurajapaksa <lahiru9954@gmail.com>
2020-03-03 21:08:52 +0000

    More rules added + brackets + storing rules in dict

--- compiler.py
+++ compiler.py
@@ -120,6 +120,7 @@ print("quantifiers =", quantifiers)
 formula = formula.strip()
 formula = formula.split(' ')
 print("formula =", formula)
+print('\n')
 
 #check whether constants, predicates and variables all have different names
 check = any(item in variables for item in constants)
@@ -136,6 +137,12 @@ nonterminals = ["variables","constants","predicates","equality","connectives","q
 validname="0123456789_"
 
 #define the production rules
+
+#define the start symbol
+print("start -> formula")
+print('\n')
+
+
 
 #define the production for 'variables'
 print("variable ->", end = " ")
@@ -186,11 +193,207 @@ print('\n')
 productiondict['constants']=constants
 
 
+
+
+#define the production for equality
+print("equality ->", end = " ")
+if len(equality)!=1:
+	print("Equality set is not equal to size 1")
+	sys.exit()
+for i in range(len(equality)):
+	currentequality = equality[i]
+	#check whether the length of the equality symbol is equal to one
+	if len(currentequality)!=1:
+		print("equality set is not equal to size one")
+		sys.exit()
+
+	for j in range(len(currentequality)):
+		if (currentequality[j]=="="):
+			continue
+		else:
+			print("\n")
+			print("incorrect equality used, must be =")
+			sys.exit()
+
+	if i==0:
+		print(equality[i], end=" ")
+		continue
+
+
+	print("|",equality[i], end=" ")
+print('\n')
+
+#store it in the dict
+productiondict['equality']=equality
+
+
+#define the production for connectives
+print("connectives ->", end = " ")
+#check that the connectives are a set of 5
+if len(connectives)!=5:
+	print("connectives set is not of size 5")
+	sys.exit()
+
+#remove the last negation element from the connectives set and store it in a variable
+#this will be used in a production rule later for //neg formula, instead of formula //neg formula which is wrong
+
+#store the last element
+negationelement = connectives[-1]
+#delete the last element now
+connectives = connectives[:-1]
+
+for i in range(len(connectives)):
+
+	currentconnective = connectives[i]
+
+
+	# for j in range(len(currentconnective)):
+	# 	if (currentconnective[j]=="="):
+	# 		continue
+	# 	else:
+	# 		print("\n")
+	# 		print("incorrect equality used, must be =")
+	# 		sys.exit()
+
+	if i==0:
+		print(currentconnective, end=" ")
+		continue
+
+
+	print("|",currentconnective, end=" ")
+print('\n')
+
+#store it in the dict
+productiondict['connectives']=connectives
+
+
+#define the production for quantifiers
+print("quantifiers ->", end = " ")
+if len(quantifiers)!=2:
+	print("quantifiers set is not of size 2")
+	sys.exit()
+for i in range(len(quantifiers)):
+	currentquantifier = quantifiers[i]
+
+	# for j in range(len(currentquantifier)):
+		# if (currentquantifier[j]=="="):
+		# 	continue
+		# else:
+		# 	print("\n")
+		# 	print("incorrect equality used, must be =")
+		# 	sys.exit()
+
+	if i==0:
+		print(currentquantifier, end=" ")
+		continue
+
+
+	print("|",currentquantifier, end=" ")
+print('\n')
+
+#store it in the dict
+productiondict['quantifiers']=quantifiers
+
+
+
+
+formulaproductions =[]
+#make the four rules + others in the spec for formulas
+
+#define the term production rule
+print("term -> variable | constant")
+termlist=['variable','constant']
+productiondict['terms'] = termlist
+print('\n')
+
+#use the term rule to define the primitive formula rule
+
+#define rule 2 (C = D), (C = x), (x = C) and (x = y) are valid
+
+print("formula -> term equality term |",end=" ")
+#get the equality set
+equals = productiondict['equality']
+rule2 = ['(','term',equality,'term',')']
+formulaproductions.append(rule2)
+
+
+#define rule 3 formula ^ formula , and so on
+#use the connectives set to define this
+# c = productiondict['connectives']
+# rule3list = []
+# print('formula -> ', end=" ")
+
+# for i in range(len(c)):
+# 	currentstring = ""
+# 	currentcon = c[i]
+# 	if i==0:
+# 		print("formula",currentcon,"formula", end=" ")
+# 		currentstring = "formula " + currentcon + " formula"
+# 		rule3list.append(currentstring)
+# 		continue
+
+# 	currentstring = "formula " + currentcon + " formula"
+# 	rule3list.append(currentstring)
+
+# 	print("|","formula",currentcon,"formula", end=" ")
+
+# productiondict['formula'] = rule3list
+#productiondict['formula'] = productiondict['formula'].extend(rule3list)
+
+
+
+#define formula connective formula
+print('formula connective formula',end=" ")
+
+connectiveset = productiondict['connectives']
+rule3 = ['(','formula',connectiveset,'formula',')']
+formulaproductions.append(rule3)
+
+
+
+
+
+#define rule 4 - there exists x formula, for all x formula as long as x is a variable
+
+
+# q = productiondict['quantifiers']
+# rule4list = []
+# #use the quantifiers set to check for stuff in front of 
+# for i in range(len(q)):
+# 	currentstring=""
+# 	currentq = q[i]
+
+# 	if i == 0:
+# 		print(currentq," variable"," formula",end=" ")
+# 		currentstring = currentq + " variable " + "formula"
+# 		rule4list.append(currentstring)
+# 		continue
+
+# 	currentstring = currentq + " variable " + "formula"
+# 	rule4list.append(currentstring)
+# 	print("|",currentq,"variable ","formula",end=" ")
+
+#productiondict['formula4'] = rule4list
+
+# productiondict['formula'] = productiondict['formula']+rule4list
+
+
+
+#define quantifier variable formula
+print('| quantifier variable formula|',end=" ")
+quantifierset = productiondict['quantifiers']
+varset = productiondict['variables']
+
+rule4 = [quantifierset,varset,'formula']
+formulaproductions.append(rule4)
+
+
+
+
 #define the production for predicates
 predicatelist= []
-print("predicates ->", end = " ")
 for i in range(len(predicates)):
-	currentpredicate = predicates[i]
+	currentpredicate = predicates[i] 
 
 	#check to see whether the predicate satisfies the minimum length: 4
 	if len(currentpredicate)<4:
@@ -258,14 +461,14 @@ for i in range(len(predicates)):
 		print(predicatename+"[",end="")
 		#'variable' repeated arity times 
 		for i in range(int(arity)-1):
-			print("variables, ",end="")
+			print("variable, ",end="")
 
 			#append to predicatestring
-			predicatestring = predicatestring + "variables,"
+			predicatestring = predicatestring + "variable,"
 
-		predicatestring = predicatestring +"variables]"
+		predicatestring = predicatestring +"variable]"
 		predicatelist.append(predicatestring)
-		print("variables]",end=" ")
+		print("variable]",end=" ")
 
 		continue
 
@@ -277,171 +480,31 @@ for i in range(len(predicates)):
 		print("variable, ",end="")
 
 		#append to predicatestring
-		predicatestring = predicatestring + "variables"
+		predicatestring = predicatestring + "variable"
 
-	predicatestring = predicatestring + "variables]"
+	predicatestring = predicatestring + "variable]"
 	predicatelist.append(predicatestring)
-	print("variables]",end=" ")
+	print("variable]",end=" ")
+
+
+#append to formulaproductions
+formulaproductions.append(predicatelist)
+
+
+#add the negation formula element
+print("| ",negationelement," formula")
+negationlist = [negationelement,'formula']
+formulaproductions.append(negationlist)
 print("\n")
 
-#store it in the dict
-productiondict['predicates']=predicatelist
-
-#define the production for equality
-print("equality ->", end = " ")
-for i in range(len(equality)):
-	currentequality = equality[i]
-
-	for j in range(len(currentequality)):
-		if (currentequality[j]=="="):
-			continue
-		else:
-			print("\n")
-			print("incorrect equality used, must be =")
-			sys.exit()
-
-	if i==0:
-		print(equality[i], end=" ")
-		continue
-
-
-	print("|",equality[i], end=" ")
-print('\n')
-
-#store it in the dict
-productiondict['equality']=equality
-
-
-#define the production for connectives
-print("connectives ->", end = " ")
-#check that the connectives are a set of 5
-if len(connectives)!=5:
-	print("connectives set is not of size 5")
-	sys.exit()
-
-for i in range(len(connectives)):
-
-	currentconnective = connectives[i]
-
-
-	# for j in range(len(currentconnective)):
-	# 	if (currentconnective[j]=="="):
-	# 		continue
-	# 	else:
-	# 		print("\n")
-	# 		print("incorrect equality used, must be =")
-	# 		sys.exit()
-
-	if i==0:
-		print(currentconnective, end=" ")
-		continue
-
-
-	print("|",currentconnective, end=" ")
-print('\n')
-
-#store it in the dict
-productiondict['connectives']=connectives
-
-
-#define the production for quantifiers
-print("quantifiers ->", end = " ")
-for i in range(len(quantifiers)):
-	currentquantifier = quantifiers[i]
-
-	# for j in range(len(currentquantifier)):
-		# if (currentquantifier[j]=="="):
-		# 	continue
-		# else:
-		# 	print("\n")
-		# 	print("incorrect equality used, must be =")
-		# 	sys.exit()
-
-	if i==0:
-		print(currentquantifier, end=" ")
-		continue
-
-
-	print("|",currentquantifier, end=" ")
-print('\n')
-
-#store it in the dict
-productiondict['quantifiers']=quantifiers
-
-#define the production for formulas
-print("formula ->", end = " ")
-
-print('\n')
-#make the four rules + others in the spec for formulas
-
-#define the term production rule
-print("term -> variable | constant")
-termlist=['variable','constant']
-productiondict['terms'] = termlist
-print('\n')
-
-#use the term rule to define the primitive formula rule
-
-#define rule 2 (C = D), (C = x), (x = C) and (x = y) are valid
-
-print("specrule2 -> term equality term")
-#get the equality set
-equals = productiondict['equality']
-rule2 = ['term',equality,'term']
-
-print(rule2)
-
-
-#define rule 3 formula ^ formula , and so on
-#use the connectives set to define this
-c = productiondict['connectives']
-rule3list = []
-print('formula -> ', end=" ")
-
-for i in range(len(c)):
-	currentstring = ""
-	currentcon = c[i]
-	if i==0:
-		print("formula",currentcon,"formula", end=" ")
-		currentstring = "formula " + currentcon + " formula"
-		rule3list.append(currentstring)
-		continue
-
-	currentstring = "formula " + currentcon + " formula"
-	rule3list.append(currentstring)
-
-	print("|","formula",currentcon,"formula", end=" ")
-
-productiondict['formula3'] = rule3list
-print('\n')
-
-#define rule 4 - there exists x formula, for all x formula as long as x is a variable
-
-print('formula ->',end=" ")
-
-q = productiondict['quantifiers']
-rule4list = []
-#use the quantifiers set to check for stuff in front of 
-for i in range(len(q)):
-	currentstring=""
-	currentq = q[i]
-
-	if i == 0:
-		print(currentq," variable"," formula",end=" ")
-		currentstring = currentq + " variable " + "formula"
-		rule4list.append(currentstring)
-		continue
-
-	currentstring = currentq + " variable " + "formula"
-	rule4list.append(currentstring)
-	print("|",currentq,"variable ","formula",end=" ")
-
-productiondict['formula4'] = rule4list
-print('\n')
-
+#add formulaproductions to the dict
+productiondict['formula']=formulaproductions
 print("dict is ", productiondict)
 
 
+
+
+#we test whether the formula is valid by trying to recreate the string with the production rules
 
 
 
